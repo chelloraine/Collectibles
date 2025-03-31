16270fb3b8ceb1e753a5b33a0cff73a006dd1ab1
chelloraine <mechelleloraine.monsale@letran.edu.ph>
2025-03-31 14:18:08 +0000

    Mar 31, 2025, 10:18 PM

--- USERPAGE/proto2.php
+++ USERPAGE/proto2.php
@@ -250,26 +250,45 @@ if (isset($_POST['change_password'])) {
 <body>
 
 <header>
-    <nav class="top-nav">
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Categories</a></li>
-            <li><a href="#">Notifications</a></li>
-            <li><a href="#">Cart</a></li>
-            <li><a href="userprofile.php">Profile</a></li>
-        </ul>
-    </nav>
+<img src="logo.png" alt="Logo" class="logo">
+        <h1>Oshi Haven</h1>
+        <div class="nav-container">
+            <nav>
+                <ul>
+                    <li><a href="#">Vtuber</a></li>
+                    <li class="dropdown">
+                        <a href="#">Products</a>
+                        <ul class="dropdown-menu">
+                            <?php foreach ($products as $name => $link): ?>
+                                <li><a href="<?php echo $link; ?>"><?php echo $name; ?></a></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                    <li><a href="#">Wishlist</a></li>
+                    <li><a href="#">Rules</a></li>
+                    <li><a href="#">About Us</a></li>
+                </ul>
+            </nav>
+            <div class="search-bar">
+                <input type="text" placeholder="Search...">
+            </div>
+
+            <div class="icons"> 
+                <div class="icon"><a href="profile.php"></a></div> 
+                <div class="icon"><a href="cart.php"></a></div>
+                <div class="icon"><a href="settings.php"></a></div>
+            </div>
+        </div>
 </header>
-
-<aside class="sidebar">
-    <ul>
-        <li><a href="#">My Orders</a></li>
-        <li><a href="#">Wishlist</a></li>
-        <li><a href="#">Messages</a></li>
-        <li><a href="#" id="account-settings-link">Account Settings</a></li>
-    </ul>
-</aside>
-
+<section class="featured-container">
+        <?php foreach ($featured_items as $item): ?>
+            <div class="featured-item">
+                <img src="<?php echo $item['image']; ?>" alt="<?php echo $item['title']; ?>">
+                <h3><?php echo $item['title']; ?></h3>
+                <p><?php echo $item['category']; ?></p>
+            </div>
+        <?php endforeach; ?>
+    </section>
 <main>
     <h1>Welcome, <?php echo htmlspecialchars($user['username']); ?>!</h1>
 
