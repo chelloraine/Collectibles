29f64c81f6c518e248060e89dd0a172a94a5996c
chelloraine <mechelleloraine.monsale@letran.edu.ph>
2025-04-01 15:30:03 +0000

    Apr 1, 2025, 11:30 PM

--- .history/USERPAGE/profile_20250401232924.php
+++ .history/USERPAGE/profile_20250401232924.php
@@ -78,7 +78,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile - Oshi Haven</title>
-    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
